8f0cbecee0682d70b3bb8fa0db93df609b54c154
IanSu18 <144109031+IanSu18@users.noreply.github.com>
2024-09-30 06:09:55 +0000

    Generates function of calculating discriminate and pre-draft

--- Ian_Sprint_Projectv1.7/Ian_Sprint_Projectv1.7.py
+++ Ian_Sprint_Projectv1.7/Ian_Sprint_Projectv1.7.py
@@ -1,1 +1,15 @@
-#initial commit
+def Calculate_Discriminat(a, b, c):
+    return b**2 - 4*a*c
+print(Calculate_Discriminat(1, -5, 6))
+  
+#discriminant = (b**2 - 4*a*c)
+
+#if discriminant == 0:
+    #print('There is one repeated real root')
+    #print()
+#if discriminant < 0:
+    #print('There is no real root (imagenary number)')
+    #print()
+#if discriminant > 0:
+    #print('There are two distinct real root')
+    #print()
