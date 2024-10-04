6cfcdb80af4c6e45a82af97ee558a9cddf51aeef
IanSu18 <144109031+IanSu18@users.noreply.github.com>
2024-10-04 06:03:14 +0000

    populate a function that calculate the roots of the given function

--- Ian_Sprint_Projectv1.7/ian_Sprint_Projectv1.75.py
+++ Ian_Sprint_Projectv1.7/ian_Sprint_Projectv1.75.py
@@ -2,15 +2,15 @@ import cmath
 
 def calculate_roots(a, b, c):
     discriminant = b**2 - 4*a*c
-    root_1 = (-b + (discriminant))/2*a
-    root_2 = (-b - (discriminant))/2*a
+    root_1 = (-b + (discriminant)**(1/2))/(2*a)
+    root_2 = (-b - (discriminant)**(1/2))/(2*a)
     return root_1, root_2
-  #  if discriminant > 0:
-
-   # elif discriminant == 0:
-
+  #if discriminant > 0:
+    #print(f'There are two real distinct roots, {root_1} and {root_2}'
+    #elif discriminant == 0:
+      #print(f'There is one real repeated root, {root_1}')
     #else:
-     #   return None
+       #return None
 
 print(calculate_roots(1, 5, 6))
 print(calculate_roots(1, 2, 1))
